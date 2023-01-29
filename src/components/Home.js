428621f06d5ba28fa35487fc8df00e81650c8015
Lipika Mohanty <110726592+Lipika07@users.noreply.github.com>
2023-01-29 18:34:36 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog">  <Link to="/blog">Go to Blog</Link></button>
+           <button id="home-to-blog">  <Link to="/blog"> Go to Blog</Link></button>
             <button id="home-to-info"> <Link to="/Info">Go to Info</Link></button>
 
         </div>
