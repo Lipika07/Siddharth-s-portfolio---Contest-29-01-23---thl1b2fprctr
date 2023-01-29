15b7acca481e63c52f93cafeb15a5b15ad9eea3f
Lipika Mohanty <110726592+Lipika07@users.noreply.github.com>
2023-01-29 18:33:45 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 const Blog = () => {
@@ -10,7 +11,7 @@ const Blog = () => {
             <h2>Checkout my awesome blogs </h2>
             <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
             <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <button id="blog-to-info"> <Link to="/Info"> Go to Info</Link></button>
+            <button id="blog-to-info">  <Link to="/Info"> Go to Info</Link></button>
             <button id="blog-to-home"> <Link to="/"> Go to Home</Link></button>
         </div>
     )
